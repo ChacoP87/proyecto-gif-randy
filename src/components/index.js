403858035f366aaf0898cdd253e80879016df6c9
Randy Santos <chacop87@gmail.com>
2023-12-06 21:13:03 -0600

    last commit xd

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-// Exporta todo desde el archivo 'AddCategory' 
-export * from './AddCategory';
-
-// Exporta todo desde el archivo 'GifGrid' 
-export * from './GifGrid';
-
-// Exporta todo desde el archivo 'GifItem' 
-export * from './GifItem';
